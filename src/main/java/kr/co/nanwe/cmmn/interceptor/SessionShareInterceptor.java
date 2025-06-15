747210a3c2b09fe6e519c3ef0a2177c56e47d6ef
전용대 <jyd30311@gmail.com>
2025-06-15 16:05:24 +0900

    test2

--- src/main/java/kr/co/nanwe/cmmn/interceptor/SessionShareInterceptor.java
+++ src/main/java/kr/co/nanwe/cmmn/interceptor/SessionShareInterceptor.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.co.nanwe.cmmn.util.SessionUtil;
-import kr.co.nanwe.login.service.LoginVO;
 
 public class SessionShareInterceptor extends HandlerInterceptorAdapter {
     
@@ -15,14 +14,46 @@ public class SessionShareInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
         
-        // 세션에서 로그인 정보 가져오기
-        LoginVO loginVO = (LoginVO) session.getAttribute(SessionUtil.LOGIN_SESSION_KEY);
+        // 전자정부 LoginVO 세션 확인
+        egovframework.com.cmm.LoginVO egovLoginVO = (egovframework.com.cmm.LoginVO) session.getAttribute("loginVO");
+        // 나눔웨 LoginVO 세션 확인
+        kr.co.nanwe.login.service.LoginVO nanweLoginVO = (kr.co.nanwe.login.service.LoginVO) session.getAttribute(SessionUtil.LOGIN_SESSION_KEY);
         
-        if (loginVO != null) {
-            // 세션 정보를 RequestContextHolder에 저장
-            SessionUtil.setSessionVal(SessionUtil.LOGIN_SESSION_KEY, loginVO);
+        if (egovLoginVO != null && nanweLoginVO == null) {
+            // 전자정부 LoginVO를 나눔웨 LoginVO로 변환
+            nanweLoginVO = convertEgovToNanwe(egovLoginVO);
+            session.setAttribute(SessionUtil.LOGIN_SESSION_KEY, nanweLoginVO);
+            SessionUtil.setSessionVal(SessionUtil.LOGIN_SESSION_KEY, nanweLoginVO);
+        } else if (nanweLoginVO != null && egovLoginVO == null) {
+            // 나눔웨 LoginVO를 전자정부 LoginVO로 변환
+            egovLoginVO = convertNanweToEgov(nanweLoginVO);
+            session.setAttribute("loginVO", egovLoginVO);
         }
         
         return true;
+    }
+    
+    private kr.co.nanwe.login.service.LoginVO convertEgovToNanwe(egovframework.com.cmm.LoginVO egovLoginVO) {
+        kr.co.nanwe.login.service.LoginVO nanweLoginVO = new kr.co.nanwe.login.service.LoginVO();
+        nanweLoginVO.setLoginId(egovLoginVO.getId());
+        nanweLoginVO.setLoginNm(egovLoginVO.getName());
+        nanweLoginVO.setLoginPw(egovLoginVO.getPassword());
+        nanweLoginVO.setEmail(egovLoginVO.getEmail());
+        nanweLoginVO.setDeptCd(egovLoginVO.getOrgnztId());
+        nanweLoginVO.setLoginIp(egovLoginVO.getIp());
+        // 필요한 다른 필드들도 추가
+        return nanweLoginVO;
+    }
+    
+    private egovframework.com.cmm.LoginVO convertNanweToEgov(kr.co.nanwe.login.service.LoginVO nanweLoginVO) {
+        egovframework.com.cmm.LoginVO egovLoginVO = new egovframework.com.cmm.LoginVO();
+        egovLoginVO.setId(nanweLoginVO.getLoginId());
+        egovLoginVO.setName(nanweLoginVO.getLoginNm());
+        egovLoginVO.setPassword(nanweLoginVO.getLoginPw());
+        egovLoginVO.setEmail(nanweLoginVO.getEmail());
+        egovLoginVO.setOrgnztId(nanweLoginVO.getDeptCd());
+        egovLoginVO.setIp(nanweLoginVO.getLoginIp());
+        // 필요한 다른 필드들도 추가
+        return egovLoginVO;
     }
 } 
